76a06c1e0ef7480997ca807f16bac5e696ca9702
nick-stuckless <156607156+nick-stuckless@users.noreply.github.com>
2024-03-12 11:48:05 -0400

    edited files

--- old_people.py
+++ old_people.py
@@ -7,6 +7,8 @@ Usage:
  python old_people.py
 """
 import os
+import sqlite3
+import pandas
 from create_db import db_path, script_dir
 
 def main():
@@ -23,8 +25,15 @@ def get_old_people():
         list: (name, age) of old people 
     """
     # TODO: Create function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    cur.execute('SELECT name,age FROM people WHERE age >= 50')
+    Data = cur.fetchall()
+    con.commit()
+    con.close()
+    
     # Hint: See example code in lab instructions entitled "Getting People Data from the Database"
-    return
+    return Data
 
 def print_name_and_age(name_and_age_list):
     """Prints name and age of all people in provided list
@@ -33,6 +42,10 @@ def print_name_and_age(name_and_age_list):
         name_and_age_list (list): (name, age) of people
     """
     # TODO: Create function body
+    for person in name_and_age_list:
+        name = person[0]
+        age = person[1]
+        print(f'{name} is {age} years old')
     # Hint: Use a for loop to iterate the list of tuples to print a sentence for each old person
     return
 
@@ -44,6 +57,8 @@ def save_name_and_age_to_csv(name_and_age_list, csv_path):
         csv_path (str): Path of CSV file
     """
     # TODO: Create function body
+    dataframe = pandas.DataFrame(name_and_age_list)
+    csv = dataframe.to_csv(csv_path)
     # Hint: In Lab 3, we converted a list of tuples into a pandas DataFrame and saved it to a CSV file
     return
 
